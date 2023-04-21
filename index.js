7858b02adfac3baa5c82636e91daaed04e561e55
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-04-21 13:50:45 -0400

    Completed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  const url = "https://anapioficeandfire.com/api/books"
+  return fetch(url).then(res => res.json()).then(books => { renderBooks(books)})
 }
 
 function renderBooks(books) {
